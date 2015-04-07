f6988c3cb162ade658be912d6462340035a18c11
eriegz <evanriegel@Evan-Riegels-MacBook-Pro.local>
2015-04-07 00:00:37 -0600

    Cleaned up and added triangles. No triangle intersection yet.

--- main.cpp
+++ main.cpp
@@ -1,36 +1,18 @@
 #include "SceneObject.h"
-#include "Sphere.h"
 #include "Plane.h"
+#include "Sphere.h"
+#include "Triangle.h"
+#include "RgbImage.h"
+
 #include "glm/glm.hpp"
 
 #include <vector>
 #include <iostream>
 using namespace std;
 
-void createTriangles(Triangle *&t1, Triangle *&t2, Triangle *&t3){
-    { //Triangle 1
-        glm::vec4 vertex1(4.0, 1.2, 3.4, 1);
-        glm::vec4 vertex2(3.5, 2.8, 4.5, 1);
-        glm::vec4 vertex3(5.5, 1.5, 5.1, 1);
-        t1 = new Triangle(vertex1, vertex2, vertex3);
-    }
-    { //Triangle 2
-        glm::vec4 vertex1(4.0, 9.4, 1.2, 1);
-        glm::vec4 vertex2(7.0, 9.8, 4.6, 1);
-        glm::vec4 vertex3(5.0, 9.1, 4.7, 1);
-        t2 = new Triangle(vertex1, vertex2, vertex3);
-    }
-    { //Triangle 3
-        glm::vec4 vertex1(0, 0, 0, 1);
-        glm::vec4 vertex2(0, 0, 0, 1);
-        glm::vec4 vertex3(0, 0, 0, 1);
-        t3 = new Triangle(vertex1, vertex2, vertex3);
-    }
-}
-
 void createScene(Plane *&pF, Plane *&pR, Plane *&pB, Plane *&pL){
-    // Create 4 planes. First, the floor:
-    { // Scoping these so I can reuse the names
+    // Scoping these so I can reuse the names
+    { // Floor
         glm::vec4 normal( 1,  0,  0, 0); //vector
         glm::vec4 point ( 0,  0,  0, 1); //point
         glm::vec4 min   ( 0,  0,  0, 1); //values
@@ -61,6 +43,46 @@ void createScene(Plane *&pF, Plane *&pR, Plane *&pB, Plane *&pL){
     }
 }
 
+void createSpheres(Sphere *&sph1, Sphere *&sph2, Sphere *&sph3, Sphere *&sph4){
+    { //Sphere1
+        glm::vec4 center(0.8, 3.5, 2.8, 1);
+        sph1 = new Sphere(center, 0.8);
+    }
+    { //Sphere 2
+        glm::vec4 center(1.5, 7.2, 3.5, 1);
+        sph2 = new Sphere(center, 2.0);
+    }
+    { //Sphere 3
+        glm::vec4 center(1.2, 4.0, 6.5, 1);
+        sph3 = new Sphere(center, 2.3);
+    }
+    { //Sphere 4
+        glm::vec4 center(2.5, 7.6, 7.8, 2.5);
+        sph4 = new Sphere(center, 2.5);
+    }
+}
+
+void createTriangles(Triangle *&t1, Triangle *&t2, Triangle *&t3){
+    { //Triangle 1
+        glm::vec4 vertex1(4.0, 1.2, 3.4, 1);
+        glm::vec4 vertex2(3.5, 2.8, 4.5, 1);
+        glm::vec4 vertex3(5.5, 1.5, 5.1, 1);
+        t1 = new Triangle(vertex1, vertex2, vertex3);
+    }
+    { //Triangle 2
+        glm::vec4 vertex1(4.0, 9.4, 1.2, 1);
+        glm::vec4 vertex2(7.0, 9.8, 4.6, 1);
+        glm::vec4 vertex3(5.0, 9.1, 4.7, 1);
+        t2 = new Triangle(vertex1, vertex2, vertex3);
+    }
+    { //Triangle 3
+        glm::vec4 vertex1(0, 0, 0, 1);
+        glm::vec4 vertex2(0, 0, 0, 1);
+        glm::vec4 vertex3(0, 0, 0, 1);
+        t3 = new Triangle(vertex1, vertex2, vertex3);
+    }
+}
+
 int main(){
     //Create the floor and walls
     Plane* pFloor;
@@ -69,6 +91,13 @@ int main(){
     Plane* pLeft;
     createScene(pFloor, pRight, pBack, pLeft);
 
+    //Create our 4 spheres
+    Sphere* sphere1;
+    Sphere* sphere2;
+    Sphere* sphere3;
+    Sphere* sphere4;
+    createSpheres(sphere1, sphere2, sphere3, sphere4);
+
     //Create our 3 triangles
     Triangle* tri1;
     Triangle* tri2;
@@ -76,6 +105,25 @@ int main(){
     createTriangles(tri1, tri2, tri3);
 
     vector<SceneObject*> objectArr;
+    //Add planes
+    objectArr.push_back(pFloor);
+    objectArr.push_back(pRight);
+    objectArr.push_back(pBack);
+    objectArr.push_back(pLeft);
+    //Add triangles
+    objectArr.push_back(tri1);
+    objectArr.push_back(tri2);
+    objectArr.push_back(tri3);
+    //Add spheres
+    objectArr.push_back(sphere1);
+    objectArr.push_back(sphere2);
+    objectArr.push_back(sphere3);
+    objectArr.push_back(sphere4);
+
+    //Define our 3 point light sources
+    glm::vec4 pointLight1(2, 13, 2, 1);
+    glm::vec4 pointLight2(7, 13, 9, 1);
+    glm::vec4 pointLight3(8, 13, 3, 1);
 
     //Define our camera position
     glm::vec4 cameraPosition(6, 5, -5, 1);
@@ -85,43 +133,8 @@ int main(){
     //Create our intersection point
     glm::vec4 intersectionPoint(0, 0, 0, 1);
 
-    /*
-    cout << "glm::dot(camera, cameraDir) = "
-         << glm::dot(glm::vec3(camera), glm::vec3(cameraDir))
-         << endl;
-    */
-
-    //Define our 3 point light sources
-    glm::vec4 pointLight1(2, 13, 2, 1);
-    glm::vec4 pointLight2(7, 13, 9, 1);
-    glm::vec4 pointLight3(8, 13, 3, 1);
-
-    /*
-    cout << "pFloor->getPointOnPlane().x = " << pFloor->getPointOnPlane().x << endl;
-    cout << "pRight->getPointOnPlane().x = " << pRight->getPointOnPlane().x << endl;
-    cout << "pBack->getPointOnPlane().x = " << pBack->getPointOnPlane().x << endl;
-    cout << "pLeft->getPointOnPlane().x = " << pLeft->getPointOnPlane().x << endl;
-    cout << "pFloor = " << pFloor << endl;
-    cout << "pRight = " << pRight << endl;
-    cout << "pBack = " << pBack << endl;
-    cout << "pLeft = " << pLeft << endl;
-    */
-
-    //Four spheres           x    y    z   rad
-    /*
-    Sphere* s1 = new Sphere(0.8, 3.5, 2.8, 0.8);
-    Sphere* s2 = new Sphere(1.5, 7.2, 3.5, 2.0);
-    Sphere* s3 = new Sphere(1.2, 4.0, 6.5, 2.0);
-    Sphere* s4 = new Sphere(2.5, 7.6, 8.3, 2.5);
-
-    objectArr.push_back(s1);
-    objectArr.push_back(s2);
-    objectArr.push_back(s3);
-    objectArr.push_back(s4);
-    */
-
+    //Iterate through our scene objects
     vector<SceneObject*>::iterator i;
-
     for(vector<SceneObject>::size_type i = 0; i != objectArr.size(); i++){
         //objectArr[i]->isIntersected();
     }
